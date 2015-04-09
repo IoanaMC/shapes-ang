0e0010932f23f1a41de21465b643a12e1589d706
Ioana Cretiu <icretiu@pitechnologies.ro>
2015-04-09 10:53:17 +0300

    Codeschoool finish lesson 1.

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function(){
+  var app = angular.module('gemStore', []);
+
+  app.controller('StoreController', function(){
+    this.products = gems;
+  });
+
+  var gems = [
+    {
+      name: 'Azurite',
+      price: 2.95,
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: 'Bloodstone',
+      price: 5.95,
+      canPurchase: true,
+      soldOut: false
+    },
+    {
+      name: 'Zircone',
+      price: 3.95,
+      canPurchase: true,
+      soldOut: false
+    },
+  ];
+
+})();
